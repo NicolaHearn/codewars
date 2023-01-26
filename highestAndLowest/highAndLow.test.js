e81c509af8fa3f93384e24a55b95818a317d9ad1
nfhearn@gmail.com <nicolahearn@MASTUDENT-Nicola-Hearn-C02G5H3VQ6L4.local>
2023-01-26 10:44:15 +0000

    passes third test - only returns characters from the input string

--- highestAndLowest/highAndLow.test.js
+++ highestAndLowest/highAndLow.test.js
@@ -11,5 +11,13 @@ describe('highAndLow', () => {
     returnValue = highAndLow("1 2 3 4 5");
 
     expect(returnValue.length).toEqual(3)
-  })
+  });
+
+  it('only returns characters from the input string', () => {
+    returnValue = highAndLow("1 2 3 4 5");
+
+    expect("1 2 3 4 5".match(returnValue.charAt(0))).not.toEqual(null);
+    expect("1 2 3 4 5".match(returnValue.charAt(2))).not.toEqual(null);
+  });
+
 });
